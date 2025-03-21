104caf45578ce13c76804b568df2178201dda794
zamriibr <zamri.ibrahim@gmail.com>
2025-03-21 22:41:40 +0800

    new api added

---
+++ api/src/functions/message.js
@@ -0,0 +1,13 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        return { 
+            jsonBody: {
+                text: "Hello, from the API!"
+            }
+        };
+    }
+});
---
+++ api/src/index.js
@@ -0,0 +1,8 @@
+const { app } = require('@azure/functions');
+
+app.setup({
+    enableHttpStream: true,
+});
+
+
+require('./functions/message');
